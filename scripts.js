1cd8e48298c60ee4cf80190a24c4038442833ac3
Michał Futera <100314711+mjfutera@users.noreply.github.com>
2023-02-18 16:04:57 +0100

    v. 1.006

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Blog template v. 1.004
+// By Michal Futera
+// https://linktr.ee/mjfutera
+
+window.onload = () => {
+    const data = new Date();
+    document.getElementById("currentYear").innerText = data.getFullYear();
+};
